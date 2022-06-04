a4b0d24c83500ae868c05ee2cfb5a346ebccdd5b
Cecilia Michell <ceciliamch7@gmail.com>
2022-06-04 15:41:54 +0700

    update smell code

--- src/before/Rent.java
+++ src/before/Rent.java
@@ -1,5 +1,29 @@
 package before;
 
+import java.sql.Date;
+
 public class Rent {
-    // data clumps
+  private String fullName;
+  private String identityNumber;
+  private Vehicle typeVehicle;
+  // data clumps, dead code
+  private Date startRent;
+  private Date endRent;
+
+  public Rent(String fullName, String identityNumber, Vehicle typeVehicle, Date startRent, Date endRent) {
+    this.fullName = fullName;
+    this.identityNumber = identityNumber;
+    this.typeVehicle = typeVehicle;
+    this.startRent = startRent;
+    this.endRent = endRent;
+  }
+
+  private static boolean validateName(String name) {
+    return name.length() >= 6 && name.length() <= 20;
+  }
+
+  private static boolean validateIdentityNumber(String identityNumber) {
+    return identityNumber.length() >= 16;
+  }
+
 }
